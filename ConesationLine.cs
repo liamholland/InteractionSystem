3b4ca9280dd54ec1e707a42e7b3e0e335ee748e1
liamholland <114741709+liamholland@users.noreply.github.com>
2023-02-20 18:29:42 +0000

    Conversation System

---
+++ ConesationLine.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class ConversationLine
+{
+    //This is the information contained in each line of a Conversation.
+    //Should be used in an array
+    public string speakerName;
+    public string speakerDialog;
+}
